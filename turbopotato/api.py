dcaa923408a742d27984148a6bf7fb31a2de6226
mattdf <work@mattdf.com>
2023-04-14 23:02:17 +0200

    add gpt generated ui

---
+++ turbopotato/api.py
@@ -0,0 +1,183 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel, Field
+from typing import List, Union, Tuple
+import json
+import requests
+import os
+
+from . import codextract
+from .openai import OPENAI_API_KEY, completion
+from .codextract import ExtractedInfo
+from .prompts import PROMPTS
+from .pipeline import workspace_for_repo, workspace_by_name
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file__), 'static')), name="static")
+
+class UrlInput(BaseModel):
+    url: str
+
+class FileList(BaseModel):
+    files: List[str]
+
+class TreeNode(BaseModel):
+    name: str
+    type: str
+    data: Union[List, None]
+    children: List[Union['TreeNode', None]] = Field(default_factory=list)
+
+class ListData(BaseModel):
+    items: List[TreeNode]
+
+TreeNode.update_forward_refs()
+
+def create_tree_node(path: str, is_directory: bool) -> Union[TreeNode, None]:
+    if is_directory:
+        children = []
+        for item in os.listdir(path):
+            child_path = os.path.join(path, item)
+            child_node = create_tree_node(child_path, os.path.isdir(child_path))
+            if child_node:
+                children.append(child_node)
+        if len(children) != 0:
+            return TreeNode(name=os.path.basename(path), type="directory", children=children)
+        return None
+    if os.path.splitext(path)[1] == ".sol":
+        return TreeNode(name=os.path.basename(path), type="file")
+
+def directory_to_list_data(dir_path: str) -> ListData:
+    if not os.path.isdir(dir_path):
+        raise ValueError(f"The provided path is not a directory: {dir_path}")
+
+    root_items = []
+    for item in os.listdir(dir_path):
+        item_path = os.path.join(dir_path, item)
+        node = create_tree_node(item_path, os.path.isdir(item_path))
+        if node:
+            root_items.append(node)
+    return ListData(items=[TreeNode(name=os.path.basename(dir_path), type="directory", children=root_items)])
+
+def create_contract_nodes(info:ExtractedInfo, lines) -> ListData:
+    contracts = []
+    for c in info.contracts:
+        children = []
+        # print('\t', 'library' if c.is_library else 'contract', c.name, c.line_start, c.line_end)
+        # for _ in c.events:  print('\t\t', _)
+        # for _ in c.using:   print('\t\t', _)
+        # for _ in c.types:   print('\t\t', _)
+        for u in c.uncaught:
+            name = u.name
+            data = [u.line_start, u.line_end]
+            children.append(TreeNode(name=name, type="uncaught", data=data, children=[]))
+        for e in c.events:
+            name = e.signature()
+            data = [e.line_start, e.line_end]
+            children.append(TreeNode(name=name, type="event", data=data, children=[]))
+        for t in c.types:
+            children.append(TreeNode(name=t, type="type", data=data, children=[]))
+        for f in c.functions:
+            name = f.signature()
+            data = [f.line_start, f.line_end]
+            children.append(TreeNode(name=name, type="function", data=data, children=[]))
+        for m in c.modifiers:
+            name = m.name
+            data = [m.line_start, m.line_end]
+            children.append(TreeNode(name=name, type="modifier", data=data, children=[]))
+        contracts.append(TreeNode(name=c.name, type="contract", data=[c.line_start, c.line_end], children=children))
+    return ListData(items=contracts)
+
+@app.get("/")
+async def serve_index_html():
+    return FileResponse(os.path.join(os.path.dirname(__file__), 'static', 'index.html'))
+
+@app.get("/favicon.ico")
+async def serve_index_html():
+    return FileResponse(os.path.join(os.path.dirname(__file__), 'static', 'favicon.ico'))
+
+@app.post("/message/")
+async def chatbot_response(message: str):
+    response = requests.post(
+        "https://api.openai.com/v1/engines/davinci-codex/completions",
+        headers={"Authorization": f"Bearer {OPENAI_API_KEY}"},
+        json={
+            "prompt": message,
+            "max_tokens": 150,
+            "n": 1,
+            "stop": None,
+            "temperature": 0.5,
+        },
+    )
+    response.raise_for_status()
+    return {"response": response.json()["choices"][0]["text"]}
+
+@app.get("/repo/{workspace_id}")
+async def workspace(workspace_id:str):
+    p = workspace_by_name(workspace_id)
+    return dict(workspace_id=workspace_id, data=directory_to_list_data(p.path('repo')))
+
+@app.post("/repo.clone/")
+async def repo_clone(repo: UrlInput):
+    sh, p = workspace_for_repo(repo.url)
+    p.git_clone()
+    return dict(workspace_id=sh, data=directory_to_list_data(p.path('repo')))
+
+@app.get("/prompts")
+async def prompts():
+    return {str(k): {
+        'description': v.description,
+        'cats': v.cats,
+        'model': v.model,
+    } for k, v in PROMPTS.items()}
+
+def _collect_files(workspace_id, file_list):
+    ws = workspace_by_name(workspace_id)
+    collected_lines = []
+    for file in file_list:
+        with ws.open('r', file) as f:
+            collected_lines += f.readlines()
+    return collected_lines
+
+@app.post("/source/{workspace_id}")
+async def source(workspace_id, file_list: FileList):    
+    collected_lines = _collect_files(workspace_id, file_list.files)
+    info = codextract.analyze_lines("CombinedContracts", collected_lines)
+    contracts_list = create_contract_nodes(info, collected_lines)
+    joined_lines = "".join(collected_lines)
+    return {"contracts": contracts_list, "lines": joined_lines}
+
+def _ranges_to_linenos(input_lines:list[int|tuple[int,int]]) -> set[int]:
+    v = []
+    for u in input_lines:
+        if isinstance(u, int):
+            v.append(u)
+        else:
+            w, x = u
+            v += list(range(w,x))
+    return set(v)
+
+class AnalyzeInput(BaseModel):
+    prompt: str
+    files: List[str]
+    lines: List[str]
+
+@app.post("/analyze/{workspace_id}")
+async def analyze(workspace_id:str, data:AnalyzeInput):
+    w = workspace_by_name(workspace_id)
+    p = PROMPTS[data.prompt]
+    v = set()
+    for _ in data.lines:
+        a, b = _.split(',')
+        v = v.union(set(range(int(a),int(b)+1)))
+    analyze_text = "".join([y for z, y in enumerate(_collect_files(workspace_id, data.files)) if z in v])
+    cache_ctx = {
+        'file_list': data.files,
+        'input_lines': data.lines,
+        'workspace_id': workspace_id,
+        'parent_cid': None
+    }
+    info = completion(p, {'CODE': analyze_text}, cache_ctx=cache_ctx)
+    with w.open('w', 'analyzer', info['cid'] + '.json') as handle:
+        handle.write(json.dumps(info))
+    return info
---
+++ turbopotato/static/main.js
@@ -0,0 +1,466 @@
+let workspace_id = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById('analyze-tab-btn').hidden = true;
+
+    // Save workspace ID in URL bar
+    const wlh = window.location.hash;
+    if( wlh != "" && wlh !== undefined ) {
+        workspace_id = wlh.slice(1);
+        load_repo(workspace_id);
+    }
+
+    loadPrompts();
+
+    async function http_get(path) {
+        const response = await fetch(path, {
+            method: "GET",
+            headers: {"Content-Type": "application/json"}
+        });
+        if (response.ok) {
+            return await response.json();
+        }
+        throw Error(response);
+    }
+
+    async function http_post(path, data) {
+        const response = await fetch(path, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(data),
+        });
+        if (response.ok) {
+            return await response.json();
+        }
+        throw Error(response);
+    }
+
+    document.getElementById("message-form").addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const messageInput = document.getElementById("message-input");
+        const message = messageInput.value.trim();
+
+        if (!message) return;
+
+        addMessageToChat("You", message);
+
+        messageInput.value = "";
+        messageInput.focus();
+
+        const response = await fetch("/message/", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ message }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            addMessageToChat("OpenAI", data.response);
+        } else {
+            addMessageToChat("Error", "Failed to get a response.");
+        }
+    });
+
+    function addMessageToChat(sender, message) {
+        const messages = document.getElementById("messages");
+        const messageDiv = document.createElement("div");
+        messageDiv.className = sender === "You" ? "user-message" : "bot-message";
+        messageDiv.innerHTML = `<strong>${sender}:</strong> ${message}`;
+        messages.appendChild(messageDiv);
+        messages.scrollTop = messages.scrollHeight;
+    }
+
+    async function loadPrompts() {        
+        const ap = document.getElementById('analyze-prompt');
+        for( const [k,v] of Object.entries(await http_get('/prompts')) ) {
+            const se = document.createElement('option');
+            se.innerHTML = k + (v.description ? ': ' + v.description : '');
+            se.setAttribute('value', k);
+            ap.appendChild(se);
+        }
+    }
+
+    async function loadRepoFromEndpoint() {
+        const loadingDiv = document.getElementById('loading-container');
+        const loadingSpinner = document.getElementById('loading-spinner');
+
+
+        // Define the keyframes for the animation
+        const keyframes = [
+            { transform: 'rotate(0deg)' },
+            { transform: 'rotate(360deg)' }
+        ];
+
+        // Define the animation properties
+        const options = {
+            duration: 2000,
+            iterations: Infinity
+        };
+
+        loadingSpinner.style.transformOrigin = 'center';
+
+        // Create the animation
+        const animation = loadingSpinner.animate(keyframes, options);
+        loadingDiv.style.display = "block";
+
+        try {
+            const url = document.getElementById("list-url-input").value;
+            const x = await http_post('/repo.clone', { url: url });
+            workspace_id = x.workspace_id;
+            window.location.hash = x.workspace_id;
+            console.log(x.data)
+            displayList(x.data.items);
+        } catch (error) {
+            alert("Error: " + error.message);
+        }
+
+        loadingDiv.style.display = "none";
+        animation.pause();
+    }
+
+    function displayList(tree, container = null) {
+        if (!container) {
+            container = document.getElementById("list-container");
+        }        
+
+        container.innerHTML = "";
+
+        const ulElement = document.createElement("ul");
+        container.appendChild(ulElement);
+
+        tree.forEach((item) => {
+            const liElement = document.createElement("li");
+
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.onchange = function () {
+                    if (item.type === "directory") {
+                    checkAllChildren(liElement, checkbox.checked);
+                }
+            };
+            liElement.appendChild(checkbox);
+
+            const label = document.createElement("label");
+            label.textContent = item.name;
+
+            const icon = document.createElement("span");
+            icon.className = item.type === "directory" ? "folder-icon" : "file-icon";
+            liElement.classList.add("data-item-" + item.type)
+            label.prepend(icon);
+
+            label.onclick = function () {
+                if (item.type === "directory") {
+                    toggleNestedListVisibility(liElement);
+                }
+            };
+            liElement.appendChild(label);
+            ulElement.appendChild(liElement);
+
+            if (item.type === "directory" && item.children && item.children.length > 0) {
+                const nestedContainer = document.createElement("div");
+                // nestedContainer.classList.add("hidden"); // Add the "hidden" class by default
+                liElement.appendChild(nestedContainer);
+                displayList(item.children, nestedContainer);
+            }
+        });
+    }
+
+    async function load_repo(name) {    
+        const response = await fetch("/repo/" + name, {
+            method: "GET",
+            headers: {"Content-Type": "application/json"}
+        });
+    
+        if (response.ok) {
+            const x = await response.json();
+            displayList(x.data.items);
+        } else {
+            alert("Failed to load the list from the endpoint.");
+        }
+    }
+
+    function displayAnalysisResult(result, container = null) {
+        if (!container) {
+            container = document.getElementById("analysis-results");
+        }
+
+        container.innerHTML = "";
+
+        const ulElement = document.createElement("ul");
+        container.appendChild(ulElement);
+
+        result.forEach((item) => {
+            const liElement = document.createElement("li");
+
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.checked = true;
+            checkbox.value = '' + item.data[0] + ',' + item.data[1];
+            checkbox.onchange = function () {
+                if (item.type === "contract") {
+                    checkAllChildren(liElement, checkbox.checked);
+                }
+            };
+            liElement.appendChild(checkbox);
+
+            const label = document.createElement("label");
+            label.textContent = item.name;
+
+            const icon = document.createElement("span");
+            if (item.type === "contract")    {
+                icon.className = "contract-icon";
+            }
+            if (item.type === "function")    {
+                icon.className = "function-icon";
+            }
+            if (item.type === "modifier")    {
+                icon.className = "gear-icon";
+            }
+            if (item.type === "event")    {
+                icon.className = "event-icon";
+            }
+            if (item.type === "type")    {
+                icon.className = "info-icon";
+            }
+            liElement.classList.add("data-item-" + item.type)
+            label.prepend(icon);
+
+            label.onclick = function () {
+                if (item.type === "contract") {
+                    toggleNestedListVisibility(liElement);
+                }
+                if (item.type !== "contract") {
+                    location.hash = "#pre-code-block." + item.data[0] + "-" + item.data[1];
+                }
+            };
+            liElement.appendChild(label);
+
+            ulElement.appendChild(liElement);
+
+            if (item.type === "contract" && item.children && item.children.length > 0) {
+                const nestedContainer = document.createElement("div");
+                // nestedContainer.classList.add("hidden"); // Add the "hidden" class by default
+                liElement.appendChild(nestedContainer);
+                displayAnalysisResult(item.children, nestedContainer);
+            }
+        });
+    }
+
+
+    function toggleNestedListVisibility(element) {
+        const nestedContainer = element.querySelector("div");
+        const nestedSpan = element.querySelector("span");
+        if (nestedContainer) {
+            nestedContainer.classList.toggle("hidden");
+            nestedSpan.classList.toggle("folder-open-icon");
+            nestedSpan.classList.toggle("folder-icon");
+        }
+    }
+    function checkAllChildren(element, checked) {
+        const checkboxes = element.querySelectorAll("input[type=checkbox]");
+        checkboxes.forEach((checkbox) => {
+            checkbox.checked = checked;
+        });
+    }
+
+    function collectCheckedFiles(container = document.getElementById("list-container"), path = []) {
+        const checkedFiles = [];
+        const ulElement = container.querySelector("ul");
+
+        ulElement.childNodes.forEach((liElement) => {
+            const checkbox = liElement.querySelector("input[type='checkbox']");
+            const label = liElement.querySelector("label");
+            const name = label.textContent;
+
+            if (checkbox.checked) {
+                if (liElement.classList.contains("data-item-file"))     {
+                    const fullPath = [...path, name].join("/");
+                    checkedFiles.push(fullPath);
+                }
+            }
+
+            const nestedContainer = liElement.querySelector("div");
+            if (nestedContainer) {
+                const nestedPath = [...path, name];
+                const nestedCheckedFiles = collectCheckedFiles(nestedContainer, nestedPath);
+                checkedFiles.push(...nestedCheckedFiles);
+            }
+        });
+
+        return checkedFiles;
+    }
+
+    async function sendCheckedFiles() {
+        if( workspace_id === null ) {
+            alert('No pipeline id!');
+            return;
+        }
+        const checkedFiles = collectCheckedFiles();
+        if( checkedFiles.length == 0 ) {
+            return;
+        }
+
+        const result = await http_post("/source/" + workspace_id, { files: checkedFiles });
+        const contracts = result.contracts;
+        const code = result.lines;
+
+        displayAnalysisResult(contracts.items);
+
+        document.getElementById("code-block").textContent = code;
+        Prism.highlightAllUnder(document.getElementById("code-editor"));
+
+        toggleTabVisibility("analyze-tab");
+        toggleTabVisibilityLeft("code-tab");
+
+        document.getElementById('analyze-tab-btn').hidden = false;
+        return result;
+    }
+
+    function toggleTabVisibility(tabId) {
+        const loadTab = document.getElementById("load-tab");
+        const analyzeTab = document.getElementById("analyze-tab");
+        const loadTabBtn = document.getElementById("load-tab-btn");
+        const analyzeTabBtn = document.getElementById("analyze-tab-btn");
+
+        if (tabId === "load-tab") {
+            loadTab.style.display = "block";
+            analyzeTab.style.display = "none";
+            loadTabBtn.classList.add("active-tab");
+            analyzeTabBtn.classList.remove("active-tab");
+        } else if (tabId === "analyze-tab") {
+            loadTab.style.display = "none";
+            analyzeTab.style.display = "block";
+            loadTabBtn.classList.remove("active-tab");
+            analyzeTabBtn.classList.add("active-tab");
+        }
+    }
+
+    function toggleTabVisibilityLeft(tabId) {
+        const chatTab = document.getElementById("chat-tab");
+        const codeTab = document.getElementById("code-tab");
+        const analTab = document.getElementById("analysis-tab");
+        const chatTabBtn = document.getElementById("chat-tab-btn");
+        const codeTabBtn = document.getElementById("code-tab-btn");
+        const analTabBtn = document.getElementById("analysis-tab-btn");
+
+        if (tabId === "chat-tab") {
+            chatTab.style.display = "flex";
+            codeTab.style.display = "none";
+            analTab.style.display = "none";
+            chatTabBtn.classList.add("active-tab");
+            codeTabBtn.classList.remove("active-tab");
+            analTabBtn.classList.remove("active-tab");
+        } else if (tabId === "code-tab") {
+            chatTab.style.display = "none";
+            codeTab.style.display = "flex";
+            analTab.style.display = "none";
+            chatTabBtn.classList.remove("active-tab");
+            codeTabBtn.classList.add("active-tab");
+            analTabBtn.classList.remove("active-tab");
+        } else if( tabId == 'analysis-tab' ) {
+            chatTab.style.display = "none";
+            codeTab.style.display = "none";
+            analTab.style.display = "flex";
+            chatTabBtn.classList.remove("active-tab");
+            codeTabBtn.classList.remove("active-tab");
+            analTabBtn.classList.add("active-tab");
+        }
+    }
+
+    function collectSelectedLines(container = document.getElementById("analysis-results")) {
+        const checkedFiles = [];
+        const ulElement = container.querySelector("ul");
+
+        ulElement.childNodes.forEach((liElement) => {
+            const checkbox = liElement.querySelector("input[type='checkbox']");
+
+            if (checkbox.checked) {
+                checkedFiles.push(checkbox.value);
+            }
+
+            const nestedContainer = liElement.querySelector("div");
+            if (nestedContainer) {
+                checkedFiles.push(...collectCheckedFiles(nestedContainer));
+            }
+        });
+
+        return checkedFiles;
+    }
+
+    function addAnalysis(result)
+    {        
+        if( document.getElementById(result.cid) ) {
+            return;
+        }
+
+        console.log(result);
+
+        const e = document.createElement('div');
+        e.setAttribute('id', result.cid);
+
+        const f = document.createElement('pre');
+        f.innerHTML = result.result.choices[0].text;
+        e.appendChild(f);
+
+        let append_to;
+        if( result.ctx.parent_cid ) {
+            append_to = document.getElementById(result.ctx.parent_cid);
+        }
+        if( append_to === undefined ) {
+            append_to = document.getElementById('analysis-tab');            
+        }
+        append_to.appendChild(e);
+    }
+
+    async function sendAnalyzeSelection() {
+        addAnalysis(await http_post("/analyze/" + workspace_id, {
+            prompt: document.getElementById('analyze-prompt').value,
+            files: collectCheckedFiles(),
+            lines: collectSelectedLines()
+        }));
+        toggleTabVisibilityLeft("analysis-tab");
+    }
+
+    document.getElementById("load-tab-btn").addEventListener("click", (event) => {
+        event.preventDefault();
+        toggleTabVisibility("load-tab");
+    });
+
+    document.getElementById("analyze-tab-btn").addEventListener("click", (event) => {
+        event.preventDefault();
+        toggleTabVisibility("analyze-tab");
+    });
+
+    document.getElementById("chat-tab-btn").addEventListener("click", (event) => {
+        event.preventDefault();
+        toggleTabVisibilityLeft("chat-tab");
+    });
+
+    document.getElementById("analysis-tab-btn").addEventListener("click", (event) => {
+        event.preventDefault();
+        toggleTabVisibilityLeft("analysis-tab");
+    });
+
+    document.getElementById("code-tab-btn").addEventListener("click", (event) => {
+        event.preventDefault();
+        toggleTabVisibilityLeft("code-tab");
+    });
+
+    document.getElementById("load-list-btn").addEventListener("click", (event) => {
+        event.preventDefault();
+        loadRepoFromEndpoint();
+    });
+
+    document.getElementById("analyze-button").addEventListener("click", (event) => {
+        event.preventDefault();
+        sendCheckedFiles();
+    });
+
+    document.getElementById("analyze-btn").addEventListener("click", (event) => {
+        event.preventDefault();
+        sendAnalyzeSelection();
+    });
+
+});
